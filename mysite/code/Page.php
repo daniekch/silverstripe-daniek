2971f01545bce07c5753e9a02af99df3d36ecfcf
Daniel Keller <daniek.ch@outlook.com>
2016-07-02 21:40:55 +0200

    Block jquery from silverstripe

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -36,8 +36,8 @@ class Page_Controller extends ContentController {
 
 	public function init() {
 		parent::init();
-		// You can include any CSS or JS required by your project here.
-		// See: http://doc.silverstripe.org/framework/en/reference/requirements
+		
+		Requirements::block('framework/thirdparty/jquery/jquery.js');
 	}
 	
 	public function googleAnalytics() {
